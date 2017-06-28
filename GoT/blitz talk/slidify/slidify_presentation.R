9a67ab293191756ca7e3c3b5a7b3a7f244ea51bd
ShirinG <shirin.glander@gmail.com>
2017-06-28 17:53:51 +0200

    slides git

--- GoT/blitz talk/slidify/slidify_presentation.R
+++ GoT/blitz talk/slidify/slidify_presentation.R
@@ -4,3 +4,5 @@ library(slidify)
 setwd("~/Documents/Github/blog_posts_prep/GoT/blitz talk/slidify/Presentation")
 slidify("index.Rmd")
 #browseURL("index.html")
+
+publish(host = "github", user = "github.com/ShirinG", repo = "GoT_slidify")
