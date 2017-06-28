c00b8e97fd0bfe9071049c50de212611f0907391
ShirinG <shirin.glander@gmail.com>
2017-06-28 17:52:45 +0200

    slides git

--- GoT/blitz talk/slidify/slidify_presentation.R
+++ GoT/blitz talk/slidify/slidify_presentation.R
@@ -1,6 +1,6 @@
 library(slidify)
-author("Presentation")
+#author("Presentation")
 
 setwd("~/Documents/Github/blog_posts_prep/GoT/blitz talk/slidify/Presentation")
 slidify("index.Rmd")
-browseURL("index.html")
+#browseURL("index.html")
